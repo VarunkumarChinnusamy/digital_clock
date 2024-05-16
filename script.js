82b1854da1998a0d40911798dacac196296fcad6
VarunkumarChinnusamy <142441646+VarunkumarChinnusamy@users.noreply.github.com>
2024-05-16 21:02:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var clock=document.querySelector(".clock");
+function runclock()
+{
+    var time=new Date();
+    var hours=time.getHours();
+    var minutes=time.getMinutes();
+    var seconds=time.getSeconds();
+    var txt="AM";
+    if(hours>12)
+        {
+            hours=hours-12;
+            txt="PM";
+        }
+        else if(hours==0)
+            {
+                hours=12;
+                txt="AM";
+            }
+    hours=hours<10?"0"+hours:hours;
+    minutes=minutes<10?"0"+minutes:minutes;
+    seconds=seconds<10?"0"+seconds:seconds;
+
+    clock.innerHTML=(`${hours}:${minutes}:${seconds}:${txt}`);        
+
+}
+runclock();
+setInterval(runclock,1000);
